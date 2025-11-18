21c245cfba455cc26c2e4d62694025ef55cc6510
Javitajonnes <javitajonnes@gmail.com>
2025-11-18 14:05:22 -0300

    first commit

---
+++ miapi/views.py
@@ -0,0 +1,17 @@
+#from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Ticket
+from .serializers import TodoSerializers
+
+class TodoListApiView(APIView):
+    parser_classes=[permissions.IsAuthenticated]
+
+    def get(self,request, *args, **kwargs):
+        todos=Ticket.objects.filter(user=request.user.id)
+        serializer=TodoSerializers(todos,many=True)
+        return Response(serializer,status=status.HTTP_200_ok)
+    
+    
